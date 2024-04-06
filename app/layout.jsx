cc6a56029f53e3dd5a7fd6465f67fd92fc09a15a
humayunrasheed <104017282+humayunrasheed@users.noreply.github.com>
2024-04-06 20:48:36 +0530

    added :

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,28 +1,33 @@
-import "@styles/globals.css";
-
-import Nav from "@components/Nav";
-import Provider from "@components/Provider";
+import '@styles/globals.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+import Head from 'next/head';
 
 export const metadata = {
-  title: "Promptopia",
-  description: "Discover & Share AI Prompts",
-};
+    title: "Quasar",
+    description: "A place to find and share writing prompts",
+}
 
-const RootLayout = ({ children }) => (
-  <html lang='en'>
-    <body>
-      <Provider>
-        <div className='main'>
-          <div className='gradient' />
-        </div>
+const RootLayout = ({ children }) => {
+    return (
+        <html lang="en">
+            <Head>
+                <link rel="icon" href="/favicon.ico" sizes="any" />
+            </Head>
+            <body>
+                <Provider>
+                    <div className="main">
+                        <div className="gradient"></div>
+                    </div>
 
-        <main className='app'>
-          <Nav />
-          {children}
-        </main>
-      </Provider>
-    </body>
-  </html>
-);
+                    <main className="app">
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
+            </body>
+        </html>
+    )
+}
 
-export default RootLayout;
+export default RootLayout
